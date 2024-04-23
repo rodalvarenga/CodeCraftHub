866ebc0fc96818b4c8b97a7d6ace38f63eab7919
Rodrigo Oliveira Alvarenga <rodalvarenga@gmail.com>
2024-04-23 14:40:58 -0400

    Fixing database connection params

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://rodrigoalva1:MjM0MDEtcm9kcmln@localhost:27017');
+    await mongoose.connect('mongodb://rodrigoalva1:MjM0MDEtcm9kcmln@rodrigoalva1-8081.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai:27017');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
